5e00fecf4fed5d5e59011ff8bae72e165f0c5bf7
Bryan Williams <Bryan.Williams@bjss.com>
2018-08-31 15:55:08 +0100

    added stroke the cat functionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ console.log('App is starting');
 Mod_Dog.bark();
 
 Mod_Cat.miaow();
+Mod_Cat.stroke(7);
 
 console.log('App has finished');
 
--- cat.js
+++ cat.js
@@ -4,6 +4,17 @@ function miaow() {
     console.log('miaow!');
 }
 
+function stroke(StrokeCount) {
+    for(let i = 0; i < StrokeCount; ++i) {
+        if(i < 5) {
+            console.log("purrrrrr");
+        } else {
+            console.log("<Scratch>*giggle*");
+        }
+    }
+}
+
 module.exports = {
-    miaow: miaow
+    miaow: miaow,
+    stroke: stroke
 };
