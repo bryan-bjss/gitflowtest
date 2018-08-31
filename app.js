c3998548b2f2e4814c395fbe080cf720b18a3126
Bryan Williams <Bryan.Williams@bjss.com>
2018-08-31 15:27:08 +0100

    Initial version with dog bark

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+  Initial version of the test app
+*/
+let Mod_Dog = require('./dog.js');
+
+console.log('App is starting');
+
+Mod_Dog.bark();
+
+console.log('App has finished');
+
---
+++ dog.js
@@ -0,0 +1,13 @@
+
+function woof() {
+    console.log('Woof Woof');
+}
+
+function bark() {
+    console.log('Bark!!');
+}
+
+module.exports = {
+    woof: woof,
+    bark: bark
+}
